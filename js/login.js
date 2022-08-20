29c6bbed674b10a64dc86503cfab4d58c4ee7281
Edgard Henrique <edigarhenriqu@gmail.com>
2022-08-20 10:19:56 -0300

    Criando cards

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 const input = document.querySelector('.login_input');
 const button = document.querySelector('.login_button');
+const form = document.querySelector('.login-form');
 
 const validateInput = ({ target }) => {
     if (target.value.length > 2) {
@@ -8,9 +9,14 @@ const validateInput = ({ target }) => {
     }
      
         button.setAttribute('disabled', '');
-    
-
 }
 
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+  localStorage.setItem('player', input.value);
+  window.location = 'pages/game.html';
+}
 
 input.addEventListener('input', validateInput);
+form.addEventListener('submit', handleSubmit);
